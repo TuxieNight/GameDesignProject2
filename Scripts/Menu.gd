2e6dd5b631ce19a51750cb498bbea1cccfd49905
TuxieNight <phowardsmith@wisc.edu>
2026-02-11 19:49:54 -0600

    updated menu screen

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -1,16 +1,6 @@
 extends Node2D
 
-func _on_Start_Button_button_down():
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), 
-								linear_to_db($HSlider.value))
-	if get_tree().change_scene_to_file("res://Scenes/Game.tscn") != OK:
-		print ("Error changing scene to Game")
-
-
-func _on_HSlider_value_changed(value):
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"),
-								linear_to_db(value))
-
-
-func _on_TextureButton_pressed():
-	$TextureButton/AudioStreamPlayer.play()
+func _unhandled_input(event):
+	if event.is_action_pressed("spacebar", false):
+		if get_tree().change_scene_to_file("res://Scenes/Game.tscn") != OK:
+			print ("Error changing scene to Game")
