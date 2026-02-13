295a58ce5ae0dd65aa8a67f13833253354bbbef1
TuxieNight <phowardsmith@wisc.edu>
2026-02-12 18:44:03 -0600

    changed victory scene to switch to menu

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -4,3 +4,4 @@ func _unhandled_input(event):
 	if event.is_action_pressed("spacebar", false):
 		if get_tree().change_scene_to_file("res://Scenes/Song1.tscn") != OK:
 			print ("Error changing scene to Game")
+ 
